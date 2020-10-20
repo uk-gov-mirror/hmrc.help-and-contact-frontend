8a074c8cdb00e67caa701d86a070a9fc561031c7
Chris Poole <CEPoole@users.noreply.github.com>
2020-10-20 13:48:03 +0100

    DL-4382 dependency uplift

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,49 +6,36 @@ private object AppDependencies {
   import play.core.PlayVersion
   import play.sbt.PlayImport._
 
-  private val govukTemplateVersion = "5.55.0-play-27"
-  private val playUiVersion = "8.12.0-play-27"
-  private val scalaTestPlusPlayVersion = "3.1.3"
-  private val pegdownVersion = "1.6.0"
-  private val mockitoAllVersion = "1.10.19"
-  private val httpCachingClientVersion = "9.1.0-play-27"
-  private val playReactivemongoVersion = "7.30.0-play-27"
-  private val playLanguageVersion = "4.3.0-play-27"
-  private val bootstrapVersion = "2.21.0"
-  private val scalacheckVersion = "1.14.3"
-  private val playPartialsVersion = "6.11.0-play-27"
-  private val wiremockVersion = "2.27.1"
-
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc" %% "simple-reactivemongo" % playReactivemongoVersion,
-    "uk.gov.hmrc" %% "govuk-template" % govukTemplateVersion,
-    "uk.gov.hmrc" %% "play-ui" % playUiVersion,
-    "uk.gov.hmrc" %% "http-caching-client" % httpCachingClientVersion,
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-27" % bootstrapVersion,
-    "uk.gov.hmrc" %% "play-language" % playLanguageVersion,
-    "uk.gov.hmrc" %% "play-partials" % playPartialsVersion,
+    "uk.gov.hmrc" %% "simple-reactivemongo" % "7.30.0-play-27",
+    "uk.gov.hmrc" %% "govuk-template" % "5.57.0-play-27",
+    "uk.gov.hmrc" %% "play-ui" % "8.12.0-play-27",
+    "uk.gov.hmrc" %% "http-caching-client" % "9.1.0-play-27",
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-27" % "2.25.0",
+    "uk.gov.hmrc" %% "play-language" % "4.4.0-play-27",
+    "uk.gov.hmrc" %% "play-partials" % "6.11.0-play-27"
   )
 
   def testCommon(): Seq[ModuleID] = {
     val scope: String = "test,it"
     Seq(
-      "org.scalatestplus.play" %% "scalatestplus-play" % scalaTestPlusPlayVersion % scope,
-      "org.pegdown" % "pegdown" % pegdownVersion % scope,
+      "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.3" % scope,
+      "org.pegdown" % "pegdown" % "1.6.0" % scope,
       "org.jsoup" % "jsoup" % "1.13.1" % scope,
       "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
-      "org.scalacheck" %% "scalacheck" % scalacheckVersion % scope
+      "org.scalacheck" %% "scalacheck" % "1.14.3" % scope
     )
   }
 
   def test(): Seq[ModuleID] = {
     val scope: String = "test"
-    Seq("org.mockito" % "mockito-all" % mockitoAllVersion % scope)
+    Seq("org.mockito" % "mockito-all" % "1.10.19" % scope)
   }
 
   def integrationTest(): Seq[ModuleID] = {
     val scope: String = "it"
-    Seq("com.github.tomakehurst" % "wiremock-jre8" % wiremockVersion % scope)
+    Seq("com.github.tomakehurst" % "wiremock-jre8" % "2.27.1" % scope)
   }
 
   def apply(): Seq[ModuleID] =
@@ -57,7 +44,7 @@ private object AppDependencies {
 }
 
 object TestPhases {
-  def oneForkedJvmPerTest(tests: Seq[TestDefinition]) =
+  def oneForkedJvmPerTest(tests: Seq[TestDefinition]): Seq[Group] =
     tests map { test =>
       new Group(
         test.name,
