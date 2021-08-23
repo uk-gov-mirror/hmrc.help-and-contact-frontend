d80bd15c2982250370175fad466a8724dfe5a04d
emmaatkins <emma.atkins@digital.hmrc.gov.uk>
2021-08-23 12:51:48 +0100

    DL-5697 ct help changes

--- test/views/ct/RegisterOrDeregisterCorporationTaxViewSpec.scala
+++ test/views/ct/RegisterOrDeregisterCorporationTaxViewSpec.scala
@@ -38,9 +38,9 @@ class RegisterOrDeregisterCorporationTaxViewSpec extends ViewBehaviours {
 
     "have correct h2 headings" in {
       val listOfHeadings: List[String] = List(
-        "How to register",
-        "Make your company active",
-        "What happens next",
+        "Register for Corporation Tax with HMRC",
+        "What you need to tell HMRC when registering for Corporation Tax",
+        "Add Corporation Tax to your business tax account",
         "Tell HMRC you are no longer trading"
       )
 
@@ -52,20 +52,31 @@ class RegisterOrDeregisterCorporationTaxViewSpec extends ViewBehaviours {
 
     "have correct content" in {
       val doc = asDocument(createView())
-      doc.text() must include("After you have registered your company with Companies House, you will need to register it for " +
-        "Corporation Tax within 3 months of starting to do business.")
-      doc.text() must include("This includes buying, selling, advertising, renting a property and employing someone. You can check " +
-        "if you’re unsure what counts as starting to do business.")
+      doc.text() must include("When you have registered your company (opens in new tab) with Companies House, you must register for Corporation Tax with HM Revenue and Customs (HMRC) within 3 months of being active.")
+      doc.text() must include("buying")
+      doc.text() must include("selling")
+      doc.text() must include("advertising")
+      doc.text() must include("renting a property")
+      doc.text() must include("employing someone")
+      doc.text() must include("similar trading activity")
+      doc.text() must include("Check if you’re unsure what counts as being active (opens in new tab).")
+      doc.text() must include("You must also register your company for Corporation Tax with HMRC if your company is registered as dormant, but is now active")
       doc.text() must include("You may get a penalty if you register late.")
-      doc.text() must include("You’ll need your company’s 10-digit Unique Taxpayer Reference (UTR) before you register for " +
-        "Corporation Tax online.")
-      doc.text() must include("This is posted to your company address by HM Revenue and Customs (HMRC), usually within a few days " +
-        "of the company being registered with Companies House (incorporated).")
+      doc.text() must include("You’ll need your company’s 10-digit Unique Taxpayer Reference (UTR).")
+      doc.text() must include("You usually get your UTR from HMRC within a few days of registering your company with Companies House.")
       doc.text() must include("Request your company’s UTR online if you did not get one after registering your company.")
-      doc.text() must include("If your company is registered as dormant and then starts trading, you need to register for Corporation Tax to tell HMRC your company is active.")
-      doc.text() must include("HMRC will tell you the deadline for paying Corporation Tax. You will need to file a Company Tax Return, " +
-        "even if you make a loss or have no Corporation Tax to pay.")
-      doc.text() must include("To stop paying Corporation Tax, you will need to close the limited company.")
+      doc.text() must include("You’ll also need:")
+      doc.text() must include("your company’s registration number (CRN)")
+      doc.text() must include("the date you started to be active - your company’s first accounting period (opens in new tab) will start from this date")
+      doc.text() must include("the date your annual accounts are made up to (the accounts preparation date)")
+      doc.getElementsByTag("article").first.getElementsByTag("h3").text() must include("What happens next")
+      doc.text() must include("HMRC will tell you the deadline for paying Corporation Tax.")
+      doc.text() must include("You must file a Company Tax Return (opens in new tab), even if you make a loss or have no Corporation Tax to pay.")
+      doc.text() must include("Add Corporation Tax to your business tax account to:")
+      doc.text() must include("file your Company Tax return online")
+      doc.text() must include("make a payment online")
+      doc.text() must include("To stop paying Corporation Tax, you must close the limited company (opens in new tab).")
+
     }
 
     "have correct links" in {
@@ -73,7 +84,7 @@ class RegisterOrDeregisterCorporationTaxViewSpec extends ViewBehaviours {
       assertLinkById(
         doc,
         "register-company-house",
-        "registered your company",
+        "registered your company (opens in new tab)",
         "https://www.gov.uk/limited-company-formation/register-your-company",
         "link - click:Register for Corporation Tax or tell HMRC you are no longer trading:registered your company",
         expectedIsExternal = true,
@@ -81,19 +92,19 @@ class RegisterOrDeregisterCorporationTaxViewSpec extends ViewBehaviours {
 
       assertLinkById(
         doc,
-        "guidance",
-        "check if you’re unsure what counts as starting to do business",
-        "https://www.gov.uk/guidance/corporation-tax-trading-and-non-trading",
-        "link - click:Register for Corporation Tax or tell HMRC you are no longer trading:check if you’re unsure what counts as starting to do business",
-        expectedIsExternal = true,
-        expectedOpensInNewTab = true)
+        "register-for-hmrc-taxes",
+        "register for Corporation Tax",
+        "http://localhost:8080/portal/business-registration/select-taxes?lang=eng",
+        "link - click:Register for Corporation Tax or tell HMRC you are no longer trading:register for Corporation Tax",
+        expectedIsExternal = false,
+        expectedOpensInNewTab = false)
 
       assertLinkById(
         doc,
-        "register-for-hmrc-taxes",
-        "register for Corporation Tax online",
-        "http://localhost:8080/portal/business-registration/select-taxes?lang=eng",
-        "link - click:Register for Corporation Tax or tell HMRC you are no longer trading:register for Corporation Tax online",
+        "check-unsure-active",
+        "Check if you’re unsure what counts as being active (opens in new tab)",
+        "https://www.gov.uk/guidance/corporation-tax-trading-and-non-trading",
+        "link - click:Register for Corporation Tax or tell HMRC you are no longer trading:check if unsure if active",
         expectedIsExternal = true,
         expectedOpensInNewTab = true)
 
@@ -108,25 +119,33 @@ class RegisterOrDeregisterCorporationTaxViewSpec extends ViewBehaviours {
 
       assertLinkById(
         doc,
-        "register_to_tell_company_active",
-        "register for Corporation Tax",
-        "http://localhost:8080/portal/business-registration/select-taxes?lang=eng",
-        "link - click:Register for Corporation Tax or tell HMRC you are no longer trading:register for Corporation Tax"
-      )
-
-      assertLinkById(
-        doc,
-        "deadline-link",
-        "Company Tax Return",
-        "https://www.gov.uk/company-tax-returns",
-        "link - click:Register for Corporation Tax or tell HMRC you are no longer trading:Company Tax Return",
+        "accounting-period",
+        "accounting period (opens in new tab)",
+        "https://www.gov.uk/corporation-tax-accounting-period",
+        "link - click:Register for Corporation Tax or tell HMRC you are no longer trading:Accounting period",
         expectedIsExternal = true,
         expectedOpensInNewTab = true)
 
       assertLinkById(
         doc,
+        "company-tax-returns",
+        "Company Tax Return (opens in new tab)",
+        "https://www.gov.uk/company-tax-returns",
+        "link - click:Register for Corporation Tax or tell HMRC you are no longer trading:Company Tax Returns",
+        expectedIsExternal = true,
+        expectedOpensInNewTab = true)
+
+      assertLinkById(
+        doc,
+        "add-ct-bta",
+        "Add Corporation Tax to your business tax account",
+        "http://localhost:8080/portal/business-registration/select-taxes?lang=eng",
+        "link - click:Register for Corporation Tax or tell HMRC you are no longer trading:Add ct to bta")
+
+      assertLinkById(
+        doc,
         "close-limited-company",
-        "close the limited company",
+        "close the limited company (opens in new tab)",
         "https://www.gov.uk/closing-a-limited-company",
         "link - click:Register for Corporation Tax or tell HMRC you are no longer trading:close the limited company",
         expectedIsExternal = true,
