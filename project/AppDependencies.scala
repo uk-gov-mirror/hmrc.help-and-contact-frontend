693c5a72668b07fbb2e812398daf9e23ba6bfd0c
peckover <56835853+peckover@users.noreply.github.com>
2024-02-23 18:24:32 +0000

    [DL-13100] upgrade play-frontend-hmrc

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -11,7 +11,7 @@ private object AppDependencies {
     "uk.gov.hmrc" %% "http-caching-client" % "10.0.0-play-28",
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "7.12.0",
     "uk.gov.hmrc" %% "tax-year" % "3.0.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc" % "6.2.0-play-28"
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-28" % "8.5.0"
   )
 
   def testCommon(): Seq[ModuleID] = {
