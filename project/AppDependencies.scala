baf6af15992ab43cdcc61d89cfcf2244e4fbcec4
kannanekambaram <112036677+kannanEkambaram@users.noreply.github.com>
2025-10-27 15:05:25 +0000

    DL-17649 : library upgrade for rebranding

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,30 +1,32 @@
 import sbt.Tests.{Group, SubProcess}
-import sbt._
+import sbt.*
 
 private object AppDependencies {
 
   import play.core.PlayVersion
-  import play.sbt.PlayImport._
+  import play.sbt.PlayImport.*
+
+  val playfrontendHmrcVersion = "12.8.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc" %% "http-caching-client-play-30" % "11.2.0",
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % "10.1.0",
-    "uk.gov.hmrc" %% "tax-year" % "4.0.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "8.5.0"
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-30"  % "10.2.0",
+    "uk.gov.hmrc" %% "tax-year"                    % "4.0.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30"  % playfrontendHmrcVersion
   )
 
   def testCommon(): Seq[ModuleID] = {
     val scope: String = "test,it"
     Seq(
-      "org.scalatestplus.play" %% "scalatestplus-play" % "7.0.1"             % scope,
-      "org.jsoup"              % "jsoup"               % "1.17.2"            % scope,
-      "uk.gov.hmrc" %% "bootstrap-test-play-30" % "10.1.0"  % scope,
-      "org.playframework"      %% "play-test"          % PlayVersion.current % scope,
-      "org.scalacheck"         %% "scalacheck"         % "1.17.0"            % scope,
-      "org.scalatestplus" %% "mockito-3-12" % "3.2.10.0" % scope,
-      "com.fasterxml.jackson.module" %% "jackson-module-scala"   % "2.15.3" % scope,
-      "com.vladsch.flexmark"   % "flexmark-all"        % "0.64.8"            % scope
+      "org.scalatestplus.play"        %% "scalatestplus-play"     % "7.0.1"             % scope,
+      "org.jsoup"                     % "jsoup"                   % "1.17.2"            % scope,
+      "uk.gov.hmrc"                   %% "bootstrap-test-play-30" % "10.2.0"            % scope,
+      "org.playframework"             %% "play-test"              % PlayVersion.current % scope,
+      "org.scalacheck"                %% "scalacheck"             % "1.17.0"            % scope,
+      "org.scalatestplus"             %% "mockito-3-12"           % "3.2.10.0"          % scope,
+      "com.fasterxml.jackson.module"  %% "jackson-module-scala"   % "2.15.3"            % scope,
+      "com.vladsch.flexmark"          % "flexmark-all"            % "0.64.8"            % scope
     )
   }
 
